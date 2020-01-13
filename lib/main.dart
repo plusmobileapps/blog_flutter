4f2948382d14eb90abab78f7ee57a34f5df2299a
Andrew Steinmetz <andrew@plusmobileapps.com>
2020-01-12 19:21:15 -0800

    refactor packages and home feed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'dart:math';
-
-import 'package:blog_flutter/SignUp.dart';
 import 'package:blog_flutter/model/user.dart';
+import 'package:blog_flutter/screens/home_feed.dart';
 import 'package:blog_flutter/services/auth.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -40,85 +37,6 @@ class MyApp extends StatelessWidget {
         ),
         home: MyHomePage(title: 'Plus Mobile Apps'),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var user = Provider.of<User>(context);
-    print('User is: $user');
-    bool isLoggedIn = user != null;
-    String toolbarText = user != null ? 'Hi ${user.displayName}' : 'Sign in/Sign up';
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            if (isLoggedIn) ...[
-              RaisedButton(
-                child: Text('Sign out'),
-                onPressed: () => AuthService().signOut(),
-              )
-            ],
-          ],
-        ),
-      ),
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-        actions: <Widget>[
-          FlatButton(
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Icon(Icons.person),
-                Text(toolbarText),
-              ],
-            ),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: SignUp(),
     );
   }
 }
---
+++ lib/model/article.dart
@@ -0,0 +1,10 @@
+class Article {
+
+  final String id;
+  final String title;
+  final String description;
+  final String minRead;
+
+  Article({this.id, this.title, this.description, this.minRead});
+
+}
